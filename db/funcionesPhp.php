c7083f59d3990a79698e187056b117013d22feb7
FFerrer <lafesdefran@gmail.com>
2018-09-22 16:46:48 +0200

    imagenes

--- db/funcionesPhp.php
+++ db/funcionesPhp.php
@@ -24,5 +24,37 @@ function selecionarTodo ($ruta,$usuario,$pass,$DB,$tabla){
     return $result;
 }
 
-//selecionarTodo('127.0.0.1','root','','cdev','trabajos');
+/* Ejemplo de como utilizar bien esta funcion
+$resultado = selecionarTodo('127.0.0.1','root','','cdev','trabajos');
+foreach($resultado as $trabajo){
+    echo $trabajo.'</br>';
+}*/
+
+function selecionarImagen($ruta, $usuario,$pass,$DB,$nombre){
+    $mysqli = new mysqli($ruta, $usuario,$pass,$DB);
+    $mysqli->set_charset("utf8");
+    
+    $res = $mysqli->query("SELECT url_video FROM trabajos WHERE titulo = '".$nombre."'");
+    
+    $result = [];
+
+    if($res != null || $res != ''){
+        while($f = $res->fetch_object()){
+            $result =$f;
+        }
+    
+    }
+    else{
+        echo 'Existe algun problema con tu base de datos';
+        echo $res;
+    }
+
+    return $result;
+}
+
+/* Ejemplo de como utilizar bien esta funcion*/
+$resultado = selecionarImagen('127.0.0.1','root','','cdev','TEST');
+foreach($resultado as $url_imagen){
+    echo "'".$url_imagen."'";
+}
 
