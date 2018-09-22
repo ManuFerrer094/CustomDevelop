6e7775abbc54601dc9794674f6b6f627983c9529
ManuFerrer <manuferrer094@gmail.com>
2018-09-22 16:02:18 +0200

    cambios

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<?php
+
+?>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!--BANNER-->
+<div class="jumbotron text-center">
+  <h1>Manuel Ferrer García </h1>
+
+  <div class="container">
+  <img class="img-responsive" src="recursos/manu.jpg" alt="Chania" width="100" height="100"> 
+</div>
+  <p>Programador multiplataforma</p>
+</div>
+<!--BANNER-->
+<div>
+    <div class="row">
+    <!--Banner Lateral-->
+        <div class="Banner Lateral col-lg-4">
+
+            <div class="col-lg-12">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+
+            <div class="col-lg-12">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+
+            <div class="col-lg-12">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+        </div>
+        <!--Banner Lateral-->
+        <!--Caja de Videos-->
+        <div class="Banner Lateral col-lg-4">
+    
+           
+                    
+    </div>
+</div>
+
+
